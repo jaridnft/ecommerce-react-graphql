ef3114e04d88eda969c4fb5c7aee1fab0561a64a
jaridwarren <jaridwarren@gmail.com>
2018-11-01 22:10:40 -0700

    graphql yoga setup

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,7 +1,7 @@
 const { GraphQLServer } = require('graphql-yoga');
 const Mutation = require('./resolvers/Mutation');
 const Query = require('./resolvers/Query');
-const { db } = require('./db');
+const db = require('./db');
 
 //  create GraphQL Yoga server
 
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // @TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
