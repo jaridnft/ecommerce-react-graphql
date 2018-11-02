5f9e54ebe13a86759e70aa7b99278b193665f96f
jaridwarren <jaridwarren@gmail.com>
2018-11-02 11:19:47 -0700

    add apollo/next integration

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,34 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
 class MyApp extends App {
+  // this is a special NextJS static method, and will run before render().
+  // This is important because we want to resolve any queries or mutations before components are rendered out onto the page.
+  // As we have server-side rendering with NextJS, we have to use this to work with Apollo
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
