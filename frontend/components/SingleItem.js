6a341564682d30329b59d510d6c7c31c8daba9b3
jaridwarren <jaridwarren@gmail.com>
2018-11-04 18:58:57 -0800

    build out single item view

---
+++ frontend/components/SingleItem.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Error from './ErrorMessage';
+import SingleItemStyles from './styles/SingleItemStyles';
+import Head from 'next/head';
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      largeImage
+    }
+  }
+`;
+
+class SingleItem extends Component {
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No item found for {this.props.id}</p>;
+          const item = data.item;
+          return (
+            <SingleItemStyles>
+              {/* Head gets inserted into HTML head by nextjs */}
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyles>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default SingleItem;
---
+++ frontend/components/styles/SingleItemStyles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const SingleItemStyles = styled.div`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
+`;
+
+export default SingleItemStyles;
