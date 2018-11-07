1757b1528f04fc97f92baab757a37c4e5dc3b13e
jaridwarren <jaridwarren@gmail.com>
2018-11-07 11:42:08 -0800

    set-up client side state management to track if cart is open or not, add UI to toggle

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,25 +1,48 @@
 import React from 'react';
-import { consolidateStreamedStyles } from 'styled-components';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
 import CloseButton from './styles/CloseButton';
 import SickButton from './styles/SickButton';
 
-const Cart = () => {
-  return (
-    <CartStyles>
-      <header>
-        <CloseButton tutke="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You Have __ Items in your cart.</p>
-      </header>
+// '@client' tag prevents from query going to server
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
 
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
-  );
-};
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
+const Cart = () => (
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => (
+          <CartStyles open={data.cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You Have __ Items in your cart.</p>
+            </header>
+
+            <footer>
+              <p>$10.10</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
+);
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import { TOGGLE_CART_MUTATION } from './Cart';
 
 const Nav = () => (
   <User>
@@ -23,6 +25,9 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+            </Mutation>
           </>
         )}
         {!me && (
