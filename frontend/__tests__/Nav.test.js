0ec7b5c4ecaefcf020a174a7ab98f5401cba9d29
jaridwarren <jaridwarren@gmail.com>
2018-11-09 17:12:57 -0800

    add Nav bar testing and sign-in redirect testing

---
+++ frontend/__tests__/Nav.test.js
@@ -0,0 +1,87 @@
+import { mount } from 'enzyme';
+import wait from 'waait';
+import toJSON from 'enzyme-to-json';
+import Nav from '../components/Nav';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { fakeUser, fakeCartItem } from '../lib/testUtils';
+
+const notSignedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: null } }
+  }
+];
+
+const signedInMocks = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: { data: { me: fakeUser() } }
+  }
+];
+
+const signedInMocksWithCartItems = [
+  {
+    request: { query: CURRENT_USER_QUERY },
+    result: {
+      data: {
+        me: {
+          ...fakeUser(),
+          cart: [fakeCartItem(), fakeCartItem(), fakeCartItem()]
+        }
+      }
+    }
+  }
+];
+
+describe('<Nav/>', async () => {
+  it('renders a minimal nav when signed out', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={notSignedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+
+    // add classes to React Components so they can be selected by test
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(toJSON(nav)).toMatchSnapshot();
+  });
+
+  it('renders full nav when signed out', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocks}>
+        <Nav />
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(nav.children().length).toBe(6);
+    expect(nav.text()).toContain('Shop');
+    expect(nav.text()).toContain('Sell');
+    expect(nav.text()).toContain('Orders');
+    expect(nav.text()).toContain('Account');
+    expect(nav.text()).toContain('Sign out');
+    expect(nav.text()).toContain('My Cart');
+  });
+
+  it('renders the amount of items in the cart', async () => {
+    const wrapper = mount(
+      <MockedProvider mocks={signedInMocksWithCartItems}>
+        <Nav />
+      </MockedProvider>
+    );
+
+    await wait();
+    wrapper.update();
+
+    const nav = wrapper.find('ul[data-test="nav"]');
+    const count = nav.find('div.count');
+    expect(toJSON(count)).toMatchSnapshot();
+  });
+});
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -9,7 +9,7 @@ import { TOGGLE_CART_MUTATION } from './Cart';
 const Nav = () => (
   <User>
     {({ data: { me } }) => (
-      <NavStyles>
+      <NavStyles data-test="nav">
         <Link href="/items">
           <a>Shop</a>
         </Link>
