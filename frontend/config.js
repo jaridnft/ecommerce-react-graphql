698628efa3e0414647d8d7363d132ff1cc7e21af
jaridwarren <jaridwarren@gmail.com>
2018-11-12 13:06:29 -0800

    changed prodEndpoint

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
-export const prodEndpoint = `https://ecommerce-react-prod.herokuapp.com/`;
+export const prodEndpoint = `https://ecomm-yoql-prod.herokuapp.com/`;
 export const perPage = 4;
