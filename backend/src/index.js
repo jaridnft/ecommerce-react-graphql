1b901a6e92ffa33cc7c878a1c96261b5ae675aca
jaridwarren <jaridwarren@gmail.com>
2018-11-12 14:25:03 -0800

    removed localhost CORS access

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: [process.env.FRONTEND_URL, 'http://localhost:7777/']
+      origin: process.env.FRONTEND_URL
     }
   },
   details =>
